267dab4871b9743130130e0101d82f186ae5b8cf
Romain FERNANDEZ <rofernan@e2r9p10.42.fr>
2020-01-30 15:04:15 +0100

    modif

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -3,11 +3,11 @@ section	.text
 _ft_strcpy:
 				xor		rdx, rdx			; put rdx to 0.
 copybyte:
-                cmp		byte[rsi + rdx], 0	; check if we are at the end of the string.
+				cmp		byte[rsi + rdx], 0	; check if we are at the end of the string.
 				je		endloop				; if yes, end of loop.
-				mov     rax, [rsi + rdx]	; rax <- value pointed by source register.
+				mov		rax, [rsi + rdx]	; rax <- value pointed by source register.
 				mov		[rdi + rdx], rax	; value pointed by destination register <- rax.
-                inc     rdx					; rdx <- rdx + 1.
+				inc		rdx					; rdx <- rdx + 1.
 				jmp		copybyte			; loop.
 endloop:
 				mov		rax, rdi			; rax <- destination string
