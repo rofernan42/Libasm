93902aaa8d91a44dbe8e7db33577f36a7fca4cec
Romain FERNANDEZ <rofernan@e2r9p10.42.fr>
2020-01-30 14:20:16 +0100

    add comments ftstrlen

--- ft_strlen.s
+++ ft_strlen.s
@@ -1,13 +1,13 @@
 global	_ft_strlen
 section	.text
 _ft_strlen:
-				xor		rax, rax
+				xor		rax, rax			; put rax to 0 (same as mov rax, 0 but more optimized).
 
 countbytes:
-				cmp		byte[rdi + rax], 0
-				je		endloop
-				inc		rax
-				jmp		countbytes
+				cmp		byte[rdi + rax], 0	; comparison between destination address and 0 (end of string).
+				je		endloop				; test if previous comparison is equal to 0. If yes, jump to endloop.
+				inc		rax					; rax <- rax + 1.
+				jmp		countbytes			; loop.
 
 endloop:
-				ret
+				ret							; end of program. Returns rax value.
