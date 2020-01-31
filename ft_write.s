41804d61d0d53eaaa2689bc39e931a9c20df7c82
Romain FERNANDEZ <rofernan@e2r9p10.42.fr>
2020-01-31 15:02:53 +0100

    add ft write

---
+++ ft_write.s
@@ -0,0 +1,12 @@
+; Check syscall_sw.h for system call write and exit.
+; The high order bits represent the class (2 is class Unix) and low order bits are the syscall number within that class.
+
+global	_ft_write
+section	.text
+_ft_write:						
+			mov rax, 0x2000004	; system call write = 4.
+			; fd value stocked in rdi.
+			; character/string stocked in rsi.
+			; nbyte value stocked in rdx.
+			syscall				; invoke the kernel.
+			ret					; returns rax (= number of characters printed).
--- main.c
+++ main.c
@@ -6,24 +6,29 @@
 /*   By: rofernan <rofernan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/30 12:05:52 by rofernan          #+#    #+#             */
-/*   Updated: 2020/01/31 11:03:31 by rofernan         ###   ########.fr       */
+/*   Updated: 2020/01/31 14:49:18 by rofernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
-int		ft_strlen(const char *str);
+size_t	ft_strlen(const char *str);
 char	*ft_strcpy(char *dst, const char *src);
 int		ft_strcmp(const char *s1, const char *s2);
+ssize_t	ft_write(int fd, const void *buf, size_t nbyte);
+ssize_t	ft_read(int fd, void *buf, size_t nbyte);
+char	*ft_strdup(const char *str);
 
 int		main(int ac, char **av)
 {
 	(void)ac;
+	(void)av;
 
 	printf("### FT_STRLEN ###\n");
 	// printf("   strlen: %d\n", (int)strlen(av[1]));
-	// printf("ft_strlen: %d\n\n", ft_strlen(av[1]));
+	// printf("ft_strlen: %d\n\n", (int)ft_strlen(av[1]));
 
 	printf("### FT_STRCPY ###\n");
 	// printf("   strcpy: %s\n", strcpy(av[1], av[2]));
@@ -31,8 +36,11 @@ int		main(int ac, char **av)
 	// printf("%c %c %c %c %c\n", av[1][0], av[1][1], av[1][2], av[1][3], av[1][4]);
 
 	printf("### FT_STRCMP ###\n");
-	printf("   strcmp: %d\n", strcmp(av[1], av[2]));
-	printf("ft_strcmp: %d\n\n", ft_strcmp(av[1], av[2]));
+	// printf("   strcmp: %d\n", strcmp(av[1], av[2]));
+	// printf("ft_strcmp: %d\n\n", ft_strcmp(av[1], av[2]));
 
+	printf("### FT_WRITE ###\n");
+	printf(" %zd\n", write(1, av[1], strlen(av[1])));
+	printf(" %zd\n", ft_write(1, av[1], strlen(av[1])));
 	return (0);
 }
